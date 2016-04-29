35f9e7f4deaf94c88f2949ffd1861fb7fdef33cd
Joerg Jung <joerg-jung@web.de>
2016-04-29 22:44:18 +0200

    get facebook user image

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,11 @@
 var express = require('express');
 var router = express.Router();
 
+// API credentials stored in separate file
+var facebookKeys = require('../keys/facebook');
+
+var facebook = require('../services/facebook') (facebookKeys.clientID, facebookKeys.clientSecret);
+
 router.use('/', function(req, res, next) {
 	// if not logged in
 	if (!req.user) {
@@ -12,9 +17,21 @@ router.use('/', function(req, res, next) {
 
 // GET users listing
 router.get('/', function(req, res) {
-	// when signed in, passport will automatically add the user object to the request object
-	// render the users template 
-	res.render('users', {user: req.user});
+
+	if (req.user.facebook) {
+		facebook.getImage(req.user.facebook.token,
+			function(results) {
+				// facebook service (getImage function above) returns results.data (the image url is inside that data object -> results.url below)
+				req.user.facebook.image = results.url;
+				res.render('users', {user: req.user});
+		});
+	}
+	else {
+		// when signed in, passport will automatically add the user object to the request object
+		// render the users template 
+		res.render('users', {user: req.user});
+	}
+	
 });
 
 module.exports = router;
---
+++ services/facebook.js
@@ -0,0 +1,29 @@
+var OAuth = require('OAuth').OAuth2;
+
+var Facebook = function(facebookKey, facebookSecret) {
+    var key = facebookKey;
+    var secret = facebookSecret;
+
+    var oauth = new OAuth(
+        key, secret, 'https://graph.facebook.com',
+        null,
+        'oauth/token',
+        null
+    );
+
+    var getImage = function(userKey, done) {
+        oauth.get(
+            'https://graph.facebook.com/v2.3/me/picture?redirect=false&type=large',
+            userKey,
+            function(err, results, res) {
+                results = JSON.parse(results);
+                done(results.data);
+            }
+        );
+    };
+    return {
+        getImage: getImage
+    };
+};
+
+module.exports = Facebook;
